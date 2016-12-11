7b376babbd716c13187b2f40068e1fbb76b3a2d5
LozaAndy <loza03081991@gmail.com>
2016-12-11 14:06:31 +0200

    add initial servlet - WelcomeController

---
+++ openSpace/src/main/java/controllers/WelcomeController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/index", "/welcome" })
+public class WelcomeController extends HttpServlet {
+	private static final long serialVersionUID = -7325170126537075555L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+	}
+
+}
