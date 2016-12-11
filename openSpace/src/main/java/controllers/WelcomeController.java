560ec1ada051fbab41dbbce836892a26efea342a
LozaAndy <loza03081991@gmail.com>
2016-12-11 19:36:28 +0200

    updated classpath and added comment

--- openSpace/src/main/java/controllers/WelcomeController.java
+++ openSpace/src/main/java/controllers/WelcomeController.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// start 
 @WebServlet(urlPatterns = {"/index.jsp","/index", "/welcome"})
 public class WelcomeController extends AbstractServlet {
 	private static final long serialVersionUID = -7325170126537075555L;
