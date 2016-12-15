6972bdc333da561e63eab34038f68b59149afbaf
LozaAndy <loza03081991@gmail.com>
2016-12-15 14:57:12 +0200

    change logic of extending in AddCommentController

--- openSpace/src/main/java/controllers/AddCommentController.java
+++ openSpace/src/main/java/controllers/AddCommentController.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns="/add-comment")
-public class AddCommentController extends AbstractServlet {
+public class AddCommentController extends HttpServlet {
 	private static final long serialVersionUID = -7625432829736695968L;
 	
 	@Override
